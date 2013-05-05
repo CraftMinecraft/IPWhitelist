ed7bfddaf595868d93fd4de94caf4746c0dbcbe2
Robin Lambertz <robinpub@googlemail.com>
2013-05-05 20:04:27 +0100

    Works now.

--- src/main/java/net/craftminecraft/bukkit/ipwhitelist/PlayerListener.java
+++ src/main/java/net/craftminecraft/bukkit/ipwhitelist/PlayerListener.java
@@ -1,80 +1,119 @@
 package net.craftminecraft.bukkit.ipwhitelist;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.util.logging.Level;
 
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.plugin.Plugin;
+
+import com.google.common.reflect.ClassPath;
 
 public class PlayerListener implements Listener {
 	IPWhitelist plugin;
+	Class clazz = null;
+	
 	public PlayerListener(IPWhitelist plugin) {
 		this.plugin = plugin;
-		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+	}
+	
+	// This is to avoid people disconnecting others. BungeeCord
+	@EventHandler
+	public void onPlayerLogin(PlayerLoginEvent ev) {
+		// If we're in a case of using bungeecord, we shouldn't let people dc others
+		if (plugin.getConfig().getBoolean("spigot_realip")) {
+			for (Player p : plugin.getServer().getOnlinePlayers()) {
+				if (ev.getPlayer().getName().equalsIgnoreCase(p.getName())) {
+					ev.disallow(PlayerLoginEvent.Result.KICK_OTHER,
+								"A player with this name is already connected to this server.");
+				}
+			}
+		// Otherwise, act like all the other plugins out there.
+		} else if (!this.plugin.getConfig().getStringList("whitelist").contains(ev.getAddress().getHostAddress())) {
+			ev.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("playerKickMessage")));
+		}
 	}
 	
 	@SuppressWarnings("unchecked")
 	@EventHandler(priority=EventPriority.HIGHEST)
-	public void onPlayerLogin(PlayerLoginEvent event) {
-		String address;
-		
+	public void onPlayerJoin(PlayerJoinEvent event) {
 		if (plugin.getConfig().getBoolean("spigot_realip")) {
-			// Get the class of the CraftPlayer.
-			Class clazz;
+			String address;
+
 			try {
-				// Already support next version *grin*
-				clazz = Class.forName("org.bukkit.craftbukkit.v1_5_R4.entity.CraftPlayer");
-			} catch (ClassNotFoundException ex0) {
-				try {
-					clazz = Class.forName("org.bukkit.craftbukkit.v1_5_R3.entity.CraftPlayer");
-				} catch (ClassNotFoundException ex1) {
-					try {
-						clazz = Class.forName("org.bukkit.craftbukkit.v1_5_R2.entity.CraftPlayer");
-					} catch (ClassNotFoundException ex2) {
-						try {
-							clazz = Class.forName("org.bukkit.craftbukkit.v1_5_R1.entity.CraftPlayer");
-						} catch (ClassNotFoundException ex3) {
-							plugin.getLogger().severe("Unsupported bukkit version. Please report to roblabla.");
-							return;
+				// Get the class of the CraftPlayer.
+				if (clazz == null) {
+					ClassPath path = ClassPath.from(event.getPlayer().getClass().getClassLoader());
+					for (ClassPath.ClassInfo classinfo : path.getTopLevelClasses()) {
+						if (classinfo.getSimpleName().equals("CraftPlayer")) {
+							clazz = Class.forName(classinfo.getName());
+							break;
 						}
 					}
-					// Should be enough for now ?
 				}
-			}
-			
-			// Get the address from CraftPlayer
-			try {
-				Object obj = clazz.getMethod("getHandle").invoke(event.getPlayer());
-				Field f = obj.getClass().getField("playerConnection");
+	
+				// Get the address from CraftPlayer
+				Object playerEntity = clazz.getMethod("getHandle").invoke(event.getPlayer());
+				
+				Field f = playerEntity.getClass().getField("playerConnection");
 				f.setAccessible(true);
-				Object playerConnection = f.get(obj);
-				f = obj.getClass().getField("networkManager");
+				Object playerConnection = f.get(playerEntity);
+				if (playerConnection == null) {
+					plugin.getLogger().log(Level.INFO, "playerConnection is null");
+					return;
+				}
+	
+				f = playerConnection.getClass().getField("networkManager");
 				f.setAccessible(true);
 				Object networkManager = f.get(playerConnection);
+				
 				Object addr = networkManager.getClass().getMethod("getSocketAddress").invoke(networkManager);
 				address = ((InetSocketAddress) addr).getAddress().getHostAddress();
+			} catch (IOException ex) {
+				plugin.getLogger().log(Level.SEVERE, "An exception has occured !", ex);
+				return;
+			} catch (ClassNotFoundException ex) {
+				plugin.getLogger().log(Level.SEVERE, "Unsupported bukkit version. Please report to roblabla.", ex);
+				return;
 			} catch (NoSuchMethodException ex) {
-				plugin.getLogger().log(Level.SEVERE, "Unsupported bukkit version. Please report to roblabla.");
+				plugin.getLogger().log(Level.SEVERE, "Unsupported bukkit version. Please report to roblabla.", ex);
 				return;
 			} catch (NoSuchFieldException ex) {
-				plugin.getLogger().log(Level.SEVERE, "Unsupported bukkit version. Please report to roblabla.");
+				plugin.getLogger().log(Level.SEVERE, "Unsupported bukkit version. Please report to roblabla.", ex);
 				return;
 			} catch (InvocationTargetException ex) {
 				plugin.getLogger().log(Level.SEVERE, "An exception has occured !", ex.getCause());
 				return;
 			} catch (Exception ex) {
-				plugin.getLogger().log(Level.SEVERE, "An exception has occured", ex);
+				plugin.getLogger().log(Level.SEVERE, "An exception has occured !", ex);
 				return;
 			}
-		} else {
-			address = event.getHostname();
+	
+			if (!this.plugin.getConfig().getStringList("whitelist").contains(address)) {
+				event.setJoinMessage(null);
+				event.getPlayer().setMetadata("ipwhitelistkick", new FixedMetadataValue(plugin, true));
+				event.getPlayer().kickPlayer(ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("playerKickMessage")));
+			}
 		}
-		if (!this.plugin.getConfig().getStringList("whitelist").contains(address))
-			event.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("playerKickMessage")));
+	}
+	
+	@EventHandler(priority=EventPriority.MONITOR)
+	public void onPlayerLeave(PlayerQuitEvent ev) {
+		if (!ev.getPlayer().getMetadata("ipwhitelistkick").isEmpty()) {
+			ev.setQuitMessage(null);
+			// Dunno if this is necessary... but I'd rather stay on the safe side
+			ev.getPlayer().removeMetadata("ipwhitelistkick", plugin);
+		}
 	}
 }
