7ed06fa9fe5a3c3c3db74f4a32ef31c2428c031c
cecile lambertz <cecile@pc0137.ct2e.btopenzone.com>
2013-05-21 15:52:22 +0100

    Use newer, better way to detect real player IP

--- src/main/java/net/craftminecraft/bukkit/ipwhitelist/IPWhitelist.java
+++ src/main/java/net/craftminecraft/bukkit/ipwhitelist/IPWhitelist.java
@@ -16,113 +16,120 @@ import org.bukkit.util.ChatPaginator.ChatPage;
 import com.google.common.collect.Lists;
 
 public class IPWhitelist extends JavaPlugin {
-	List<String> bungeeips = Lists.newArrayList();
 
-	public void onEnable() {
-		getConfig().options().copyDefaults(true);
-		saveDefaultConfig();
-		this.getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
-		Configuration bukkityml = YamlConfiguration.loadConfiguration(new File(this.getDataFolder().getParentFile().getParentFile(), "bukkit.yml"));
-		bungeeips.addAll(bukkityml.getStringList("settings.bungee-proxies"));
-	}
+    List<String> bungeeips = Lists.newArrayList();
 
-	public void onDisable() { bungeeips.clear(); }
+    public void onEnable() {
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
+        this.getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+        Configuration bukkityml = YamlConfiguration.loadConfiguration(new File(this.getDataFolder().getParentFile().getParentFile(), "bukkit.yml"));
+        bungeeips.addAll(bukkityml.getStringList("settings.bungee-proxies"));
+    }
 
-	public List<String> getBungeeIPs() {
-	    return this.bungeeips;
-	}
+    public void onDisable() {
+        bungeeips.clear();
+    }
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (!command.getName().equalsIgnoreCase("ipwhitelist")) {
-			return false;
-		}
-		if (!sender.hasPermission("ipwhitelist.setup")) {
-			sender.sendMessage(ChatColor.RED + "You do not have permission to perform this action.");
-			return true;
-		}
-		if (args.length == 0) {
-			sender.sendMessage(getTag() + ChatColor.AQUA + "Commands : ");
-			sender.sendMessage(ChatColor.AQUA + "/ipwhitelist list [page] - List whitelisted IPs");
-			sender.sendMessage(ChatColor.AQUA + "/ipwhitelist addip <ip> - Add IP to whitelist");
-			sender.sendMessage(ChatColor.AQUA + "/ipwhitelist remip <ip> - Removes IP to whitelist");
-			sender.sendMessage(ChatColor.AQUA + "/ipwhitelist reload - Reload whitelist");
-			return true;
-		}
+    public List<String> getBungeeIPs() {
+        return this.bungeeips;
+    }
 
-		if (args[0].equalsIgnoreCase("list")) {
-			sender.sendMessage(getTag() + ChatColor.AQUA + "Whitelisted IPs :");
-			StringBuffer iplistbuff = new StringBuffer();
-			for (String ip : bungeeips) {
-				iplistbuff.append(ChatColor.AQUA + ip + "\n");
-			}
-			for (String ip : getConfig().getStringList("whitelist")) {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!command.getName().equalsIgnoreCase("ipwhitelist")) {
+            return false;
+        }
+        if (!sender.hasPermission("ipwhitelist.setup")) {
+            sender.sendMessage(ChatColor.RED + "You do not have permission to perform this action.");
+            return true;
+        }
+        if (args.length == 0) {
+            sender.sendMessage(getTag() + ChatColor.AQUA + "Commands : ");
+            sender.sendMessage(ChatColor.AQUA + "/ipwhitelist list [page] - List whitelisted IPs");
+            sender.sendMessage(ChatColor.AQUA + "/ipwhitelist addip <ip> - Add IP to whitelist");
+            sender.sendMessage(ChatColor.AQUA + "/ipwhitelist remip <ip> - Removes IP to whitelist");
+            sender.sendMessage(ChatColor.AQUA + "/ipwhitelist reload - Reload whitelist");
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("list")) {
+            sender.sendMessage(getTag() + ChatColor.AQUA + "Whitelisted IPs :");
+            StringBuffer iplistbuff = new StringBuffer();
+            for (String ip : bungeeips) {
                 iplistbuff.append(ChatColor.AQUA + ip + "\n");
             }
-			// Delete last newline if there is one.
-			if (iplistbuff.length() > 0) {
-				iplistbuff.deleteCharAt(iplistbuff.length()-1);
-			}
-			String iplist = iplistbuff.toString();
-			ChatPage page;
-			if (args.length > 1) {
-				page = ChatPaginator.paginate(iplist, Integer.parseInt(args[1]), ChatPaginator.GUARANTEED_NO_WRAP_CHAT_PAGE_WIDTH, ChatPaginator.CLOSED_CHAT_PAGE_HEIGHT-2);
-				sender.sendMessage(page.getLines());
-			} else {
-				page = ChatPaginator.paginate(iplist, 1, ChatPaginator.GUARANTEED_NO_WRAP_CHAT_PAGE_WIDTH, ChatPaginator.CLOSED_CHAT_PAGE_HEIGHT-2);
-				sender.sendMessage(page.getLines());
-			}
-			sender.sendMessage(ChatColor.AQUA + "Page " + page.getPageNumber() + "/" + page.getTotalPages() + ".");
-			return true;
-		}
-		if (args[0].equalsIgnoreCase("addip")) {
-			if (args.length < 2) {
-				sender.sendMessage(getTag() + ChatColor.AQUA + "Command usage : ");
-				sender.sendMessage(ChatColor.AQUA + "/ipwhitelist addip <ip>");
-				return true;
-			}
-			if (!bungeeips.contains(args[1])
-			    && !getConfig().getStringList("whitelist").contains(args[1])) {
-			    List<String> whitelist = getConfig().getStringList("whitelist");
-			    whitelist.add(args[1]);
-				getConfig().set("whitelist", whitelist);
-				this.saveConfig();
-				sender.sendMessage(getTag() + ChatColor.AQUA + "Successfully whitelisted IP " + args[1] + "!");
-				return true;
-			}
-			sender.sendMessage(getTag() + ChatColor.AQUA + "IP " + args[1] + " was already whitelisted!");
-			return true;
-		}
-		if (args[0].equalsIgnoreCase("remip")) {
-			if (args.length < 2) {
-				sender.sendMessage(getTag() + ChatColor.AQUA + "Command usage : ");
-				sender.sendMessage(ChatColor.AQUA + "/ipwhitelist remip <ip>");
-				return true;
-			}
-			List<String> whitelist = getConfig().getStringList("whitelist");
-			if (whitelist.remove(args[1])) {
-				getConfig().set("whitelist", whitelist);
-				this.saveConfig();
-				sender.sendMessage(getTag() + ChatColor.AQUA + "Successfully unwhitelisted IP " + args[1] + "!");
-				return true;
-			}
-			sender.sendMessage(getTag() + ChatColor.AQUA + "IP " + args[1] + " was not whitelisted!");
-			return true;
-		}
-		if (args[0].equalsIgnoreCase("reload")) {
-			this.reloadConfig();
-			sender.sendMessage(getTag() + ChatColor.AQUA + "Successfully reloaded config!");
-			return true;
-		}
-		sender.sendMessage(getTag() + ChatColor.AQUA + "Commands : ");
-		sender.sendMessage(ChatColor.AQUA + "/ipwhitelist list [page] - List whitelisted IPs");
-		sender.sendMessage(ChatColor.AQUA + "/ipwhitelist addip <ip> - Add IP to whitelist");
-		sender.sendMessage(ChatColor.AQUA + "/ipwhitelist remip <ip> - Removes IP to whitelist");
-		sender.sendMessage(ChatColor.AQUA + "/ipwhitelist reload - Reload whitelist");
-		return true;
-	}
-	
-	public String getTag() {
-		return ChatColor.ITALIC.toString() + ChatColor.GREEN + "[" + ChatColor.AQUA + this.getName() + ChatColor.GREEN + "] " + ChatColor.RESET;
-	}
+            for (String ip : getConfig().getStringList("whitelist")) {
+                iplistbuff.append(ChatColor.AQUA + ip + "\n");
+            }
+            // Delete last newline if there is one.
+            if (iplistbuff.length() > 0) {
+                iplistbuff.deleteCharAt(iplistbuff.length() - 1);
+            }
+            String iplist = iplistbuff.toString();
+            ChatPage page;
+            if (args.length > 1) {
+                page = ChatPaginator.paginate(iplist, Integer.parseInt(args[1]), ChatPaginator.GUARANTEED_NO_WRAP_CHAT_PAGE_WIDTH, ChatPaginator.CLOSED_CHAT_PAGE_HEIGHT - 2);
+                sender.sendMessage(page.getLines());
+            } else {
+                page = ChatPaginator.paginate(iplist, 1, ChatPaginator.GUARANTEED_NO_WRAP_CHAT_PAGE_WIDTH, ChatPaginator.CLOSED_CHAT_PAGE_HEIGHT - 2);
+                sender.sendMessage(page.getLines());
+            }
+            sender.sendMessage(ChatColor.AQUA + "Page " + page.getPageNumber() + "/" + page.getTotalPages() + ".");
+            return true;
+        }
+        if (args[0].equalsIgnoreCase("addip")) {
+            if (args.length < 2) {
+                sender.sendMessage(getTag() + ChatColor.AQUA + "Command usage : ");
+                sender.sendMessage(ChatColor.AQUA + "/ipwhitelist addip <ip>");
+                return true;
+            }
+            if (!bungeeips.contains(args[1])
+                    && !getConfig().getStringList("whitelist").contains(args[1])) {
+                List<String> whitelist = getConfig().getStringList("whitelist");
+                whitelist.add(args[1]);
+                getConfig().set("whitelist", whitelist);
+                this.saveConfig();
+                sender.sendMessage(getTag() + ChatColor.AQUA + "Successfully whitelisted IP " + args[1] + "!");
+                return true;
+            }
+            sender.sendMessage(getTag() + ChatColor.AQUA + "IP " + args[1] + " was already whitelisted!");
+            return true;
+        }
+        if (args[0].equalsIgnoreCase("remip")) {
+            if (args.length < 2) {
+                sender.sendMessage(getTag() + ChatColor.AQUA + "Command usage : ");
+                sender.sendMessage(ChatColor.AQUA + "/ipwhitelist remip <ip>");
+                return true;
+            }
+            List<String> whitelist = getConfig().getStringList("whitelist");
+            if (whitelist.remove(args[1])) {
+                getConfig().set("whitelist", whitelist);
+                this.saveConfig();
+                sender.sendMessage(getTag() + ChatColor.AQUA + "Successfully unwhitelisted IP " + args[1] + "!");
+                return true;
+            }
+            if (bungeeips.contains(args[1])) {
+                sender.sendMessage(getTag() + ChatColor.AQUA + "IP " + args[1] + " is in your bukkit.yml's bungee-proxies. Remove it there!");
+                return true;
+            }
+            sender.sendMessage(getTag() + ChatColor.AQUA + "IP " + args[1] + " was not whitelisted!");
+            return true;
+        }
+        if (args[0].equalsIgnoreCase("reload")) {
+            this.reloadConfig();
+            sender.sendMessage(getTag() + ChatColor.AQUA + "Successfully reloaded config!");
+            return true;
+        }
+        sender.sendMessage(getTag() + ChatColor.AQUA + "Commands : ");
+        sender.sendMessage(ChatColor.AQUA + "/ipwhitelist list [page] - List whitelisted IPs");
+        sender.sendMessage(ChatColor.AQUA + "/ipwhitelist addip <ip> - Add IP to whitelist");
+        sender.sendMessage(ChatColor.AQUA + "/ipwhitelist remip <ip> - Removes IP to whitelist");
+        sender.sendMessage(ChatColor.AQUA + "/ipwhitelist reload - Reload whitelist");
+        return true;
+    }
+
+    public String getTag() {
+        return ChatColor.ITALIC.toString() + ChatColor.GREEN + "[" + ChatColor.AQUA + this.getName() + ChatColor.GREEN + "] " + ChatColor.RESET;
+    }
 }
--- src/main/java/net/craftminecraft/bukkit/ipwhitelist/PlayerListener.java
+++ src/main/java/net/craftminecraft/bukkit/ipwhitelist/PlayerListener.java
@@ -1,5 +1,6 @@
 package net.craftminecraft.bukkit.ipwhitelist;
 
+import java.util.logging.Level;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -7,22 +8,25 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 
 public class PlayerListener implements Listener {
-	IPWhitelist plugin;
-	public PlayerListener(IPWhitelist plugin) {
-		this.plugin = plugin;
-		plugin.getServer().getPluginManager().registerEvents(this, plugin);
-	}
-	
-	@EventHandler(priority=EventPriority.HIGHEST)
-	public void onPlayerLogin(PlayerLoginEvent event) {
-	    String address;
-	    try {
-	        address = event.getPlayer().getRawAddress().getAddress().getHostAddress();
-	    } catch (NoSuchMethodError err) {
-	        address = event.getAddress().getHostAddress();
-	    }
-		if (!this.plugin.getConfig().getStringList("whitelist").contains(address)
-		    && !this.plugin.getBungeeIPs().contains(address))
-			event.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("playerKickMessage")));
-	}
+
+    IPWhitelist plugin;
+
+    public PlayerListener(IPWhitelist plugin) {
+        this.plugin = plugin;
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerLogin(PlayerLoginEvent event) {
+        String address;
+        try {
+            address = event.getRealAddress().getHostAddress();
+        } catch (NoSuchMethodError err) {
+            address = event.getAddress().getHostAddress();
+        }
+        if (!this.plugin.getConfig().getStringList("whitelist").contains(address)
+                && !this.plugin.getBungeeIPs().contains(address)) {
+            event.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("playerKickMessage")));
+        }
+    }
 }
